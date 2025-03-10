7df3f31680a0584c381d06c13631b84c7356cec0
MunyengwaNoel <nmunyengwa2019@gmail.com>
2025-03-10 16:10:43 +0200

    clean up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,12 +9,12 @@ class UserController extends Controller
     public static function welcome(){
         return "Welcome to RentOra Housing.\n1. Login\n2. Register";
     }
-    public static function register(Request $request):array{
-        $data = ["message"=>"Enter Your Name:",
-            "next"=> ""
-    ];
-    return $data;
-    }
+    // public static function register(Request $request):array{
+    //     $data = ["message"=>"Enter Your Name:",
+    //         "next"=> ""
+    // ];
+    // return $data;
+    // }
 
     public static function login(){
         return "Enter Password";
